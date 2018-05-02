0f8d73c573a61f50087d4bf7363c9868f13e5bc1
Leonardo Geus <leonardodegeus@gmail.com>
2018-05-02 12:14:03 -0300

    Updated fibonacci file

---
+++ Fibonacci.swift
@@ -0,0 +1,14 @@
+//
+//  Fibonacci.swift
+//
+//  Created by Leonardo Geus on 02/05/2018.
+//  Copyright © 2018 T-Systems do Brasil Ltda. All rights reserved.
+//
+
+import UIKit
+
+var n = 22
+
+func fibonacciSequence(n:Int) -> [Int] {
+    return []
+}
